1bc87dd737ee6e91cbc989706616c321a6967eba
genryuusay <ravilx02@gmail.com>
2023-08-30 10:55:10 -0300

    commit inicial

---
+++ src/srp/shooping-cart.ts
@@ -0,0 +1,62 @@
+type CartItem = {
+  name: string;
+  price: number;
+};
+
+export class ShoopingCart {
+  private readonly _items: CartItem[] = [];
+  private orderStatus: 'open' | 'closed' = 'open';
+
+  addItem(item: CartItem): void {
+    this._items.push(item);
+  }
+
+  removeItem(index: number): void {
+    this._items.splice(index, 1);
+  }
+
+  get items(): Readonly<CartItem[]> {
+    return this._items;
+  }
+
+  total(): number {
+    return +this._items
+      .reduce((total, next) => total + next.price, 0)
+      .toFixed(2);
+  }
+  checkout(): void {
+    if (this.isEmpty()) {
+      console.log('Seu carrinho está vazio');
+      return;
+    }
+
+    this.orderStatus = 'closed';
+    this.sendMessage(`Seu pedido com total de ${this.total()} foi recebido.`);
+    this.saveOrder();
+    this.clear();
+  }
+
+  isEmpty(): boolean {
+    return this._items.length === 0;
+  }
+
+  sendMessage(msg: string): void {
+    console.log('Mensagem enviada: ', msg);
+  }
+  saveOrder(): void {
+    console.log('Pedido salvo com sucesso...');
+  }
+  clear(): void {
+    console.log('Carrinho de compras foi limpo...');
+    this._items.length = 0;
+  }
+}
+
+const shoopingCart = new ShoopingCart();
+
+shoopingCart.addItem({ name: 'Camiseta', price: 49.91 });
+shoopingCart.addItem({ name: 'Caderno', price: 9.911 });
+shoopingCart.addItem({ name: 'Lápis', price: 1.59 });
+
+console.log(shoopingCart.items);
+console.log(shoopingCart.total());
